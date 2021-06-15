dd18b3dfc2a338540c5f8b1168ccd06064f4cff8
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2021-06-16 00:38:04 +0500

    update addpp.js

--- src/App.js
+++ src/App.js
@@ -4,36 +4,10 @@ import "./index.css";
 import logo from "./logo.png";
 
 const App = () => {
-  let a = 1;
-  let newTime = new Date().toLocaleTimeString();
 
-  const [time, ctime] = useState(newTime)
-
-  const updateTime = () => {
-    let Time = new Date().toLocaleTimeString();
-    console.log(Time);
-    ctime(Time);
-    // updateTime()
-  }
-  setInterval(updateTime, 1000)
   return (
-    <div >
-      <div className="row">
-        <div className="col-md-4">
-          <MDBCard color="indigo" text="white" className="text-center">
-            <MDBCardBody>{time}</MDBCardBody>
-          </MDBCard>
-          <div className="text-center">
-            <MDBBtn social="comm" onClick={updateTime}>
-              <MDBIcon icon="comments" className="pr-1" /> UpdateTime
-        </MDBBtn>
-          </div>
-        </div>
-
-      </div>
-
-
-
+    <div>
+      <h1>Hellow World</h1>
     </div>
 
   )
